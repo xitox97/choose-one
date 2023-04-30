0b72b973e63d0c05028ed4e4250e8c18ecd888da
Farhan Hadi <farhan.abdhadi@gmail.com>
2023-05-01 01:25:31 +0800

    Added basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,73 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    const [options, setOptions] = useState([""])
+    console.log("op", options)
+    const handleAdd = () => {
+        setOptions((prev) => [...prev, ""])
+    }
+
+    const handleRemove = (index: number) => {
+        console.log(index, options)
+        console.log(options.filter((item, i) => i !== index))
+        setOptions(options.filter((item, i) => i !== index))
+    }
+
+    const handleOnBlurInput = (e: any, index: number) => {
+        const data = options.map((option, i) => {
+            if (i === index) {
+                return e.target.value
+            }
+
+            return option
+        })
+
+        setOptions(data)
+    }
+    return (
+        <main className="min-h-screen bg-white flex justify-center">
+            <div className="w-3/4">
+                <div>
+                    <input
+                        name="title"
+                        type="text"
+                        autoComplete="false"
+                        className="w-2/4 text-yellow-400 border-b-2 border-black bg-transparent text-center text-3xl outline-none"
+                    />
+                </div>
+                <div className="mt-10 flex flex-col space-y-4 bg-red-400">
+                    {options &&
+                        options.map((option, index) => (
+                            <div className="">
+                                <input
+                                    key={index}
+                                    onBlur={(e) => handleOnBlurInput(e, index)}
+                                    name="options[]"
+                                    type="text"
+                                    className="bg-red-300 border-b-8 border-r-8 border-l-2 border-t-2 border-purple-800 rounded-lg p-4"
+                                />
+
+                                <button
+                                    className="bg-blue-400 rounded-full p-1 ml-2"
+                                    onClick={() => handleRemove(index)}
+                                >
+                                    ➖
+                                </button>
+                                {options.length === index + 1 && (
+                                    <button
+                                        className="bg-pink-400 rounded-full p-1 ml-2"
+                                        onClick={handleAdd}
+                                    >
+                                        ➕
+                                    </button>
+                                )}
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default App
